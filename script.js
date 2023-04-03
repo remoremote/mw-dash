bc018350d56a20354245c29ae7c2402e2de128dd
Remo <81964236+remoremote@users.noreply.github.com>
2023-04-03 11:35:17 +0200

    code improvement

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const kpis = loadKpis();
+document.addEventListener("DOMContentLoaded", () => {
+  const kpis = loadKpis();
+  const gridContainer = document.querySelector(".grid-container");
+  const kpiTemplate = document.getElementById("kpi-template");
+
+  kpis.forEach((kpi, index) => {
+    renderKpi(gridContainer, kpiTemplate, kpi);
+  });
+});
 
 function loadKpis() {
   const storedKpis = localStorage.getItem("kpis");
   if (storedKpis) {
     return JSON.parse(storedKpis);
-  } else {
-    return [
-      { title: "Number of Listings", currentValue: 148, goal: 2000 },
-      { title: "User Sign-ups", currentValue: 403, goal: 1000 },
-      { title: "Conversion Rate", currentValue: "8%", goal: "25%" },
-      { title: "Average Revenue per User (ARPU)", currentValue: "896", goal: "2000" },
-      { title: "Gross Transaction Volume (GTV)", currentValue: "52974", goal: "250000" },
-      { title: "Total Revenue Year-to-date (YTD)", currentValue: "12595", goal: "172000" },
-    ];
   }
+
+  return getDefaultKpis();
 }
 
-function saveKpis() {
+function getDefaultKpis() {
+  return [
+    { title: "Number of Listings", currentValue: 148, goal: 2000 },
+    { title: "User Sign-ups", currentValue: 403, goal: 1000 },
+    { title: "Conversion Rate", currentValue: "8%", goal: "25%" },
+    { title: "Average Revenue per User (ARPU)", currentValue: "896", goal: "2000" },
+    { title: "Gross Transaction Volume (GTV)", currentValue: "52974", goal: "250000" },
+    { title: "Total Revenue Year-to-date (YTD)", currentValue: "12595", goal: "172000" },
+  ];
+}
+
+function saveKpis(kpis) {
   localStorage.setItem("kpis", JSON.stringify(kpis));
 }
 
-const gridContainer = document.querySelector(".grid-container");
-const kpiTemplate = document.getElementById("kpi-template");
-
-kpis.forEach((kpi, index) => {
+function renderKpi(gridContainer, kpiTemplate, kpi) {
   const kpiClone = kpiTemplate.content.cloneNode(true);
+
+  // Assign values to the KPI elements
   const title = kpiClone.querySelector(".kpi-title");
   const progressBar = kpiClone.querySelector(".kpi-progress-bar");
   const currentValue = kpiClone.querySelector(".current-value");
@@ -37,6 +48,7 @@ kpis.forEach((kpi, index) => {
   goalValue.innerText = kpi.goal;
   progressBar.style.width = calculatePercentage(kpi.currentValue, kpi.goal) + "%";
 
+  // Update KPI values and save them to local storage
   saveButton.addEventListener("click", () => {
     const newValue = input.value;
     if (newValue) {
@@ -44,12 +56,12 @@ kpis.forEach((kpi, index) => {
       currentValue.innerText = newValue;
       progressBar.style.width = calculatePercentage(newValue, kpi.goal) + "%";
       input.value = "";
-      saveKpis();
+      saveKpis(kpis);
     }
   });
 
   gridContainer.appendChild(kpiClone);
-});
+}
 
 function calculatePercentage(currentValue, goal) {
   const current = parseFloat(currentValue);
